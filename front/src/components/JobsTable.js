ed52f7588a318a6cd260ffd78f2bae19c207a11b
Mihalache Andrei <andreimircea.mihalache@gmail.com>
2022-02-07 14:15:59 +0200

    final

--- front/src/components/JobsTable.js
+++ front/src/components/JobsTable.js
@@ -16,57 +16,114 @@ function JobsTable() {
   const jobpostingList = useSelector(jobpostingSelector);
   const candidateList = useSelector(candidateSelector);
 
+  const [selectedId, setSelectedId] = useState(0);
+
   const [postingId, setPostingId] = useState(0);
   const [postingDescription, setPostingDescription] = useState("");
   const [postingDeadline, setPostingDeadline] = useState({ date: new Date() });
 
-  const dispatch = useDispatch();
-  const [selectedId, setSelectedId] = useState(0);
+  const [candidateId, setCandidateId] = useState(0);
+  const [candidateName, setCandidateName] = useState("");
+  const [candidateEmail, setCandidateEmail] = useState("");
+  const [candidateCv, setCandidateCv] = useState("");
+  const [candidateJobId, setCandidateJobId] = useState(0);
+  const [isNewCandidate, setIsNewCandidate] = useState(false);
+  const [asc, setAsc] = useState(0);
   const [listDialog, setDialog] = useState(false);
   const [addDialog, setAddDialog] = useState(false);
   const [isNewRecord, setIsNewRecord] = useState(false);
+  const [cvDialog, setCvDialog] = useState(false);
+  const [cv, setCv] = useState("");
+  const [candidateDialog, setCandidateDialog] = useState(false);
+  const [filter, setFilter] = useState("");
+
+  const dispatch = useDispatch();
+
+  const setDeadline = (value) => {
+    setPostingDeadline({ date: value });
+  };
 
   useEffect(() => {
     dispatch(jobpostingActions.getJobPostings());
   }, [dispatch]);
 
-  const renderDialog = () => {
-    if (candidateList.length > 0) {
-      return (
-        <DataTable value={candidateList} responsiveLayout="scroll">
-          <Column field="name" header="Name"></Column>
-          <Column field="role" header="Role"></Column>
-          <Column body={opsDetail}></Column>
-        </DataTable>
-      );
-    } else {
-      return <h3>Entitatea nu are copii</h3>;
-    }
+  const hideCv = () => {
+    setCvDialog(false);
+    setDialog(true);
   };
+  const openCV = (rowData) => {
+    setCv(rowData.cv);
+    setCvDialog(true);
+  };
+  const renderCV = () => {
+    return <div>{cv}</div>;
+  };
+
+  const hideCandidate = () => {
+    setCandidateDialog(false);
+  };
+  const showCandidate = (rowData) => {
+    setCandidateId(rowData.id);
+    setCandidateName(rowData.name);
+    setCandidateEmail(rowData.email);
+    setCandidateCv(rowData.cv);
+    setCandidateJobId(rowData.jobPostingId);
+    setCandidateDialog(true);
+    setIsNewCandidate(false);
+  };
+
+  const addCandidate = (rowData) => {
+    setCandidateId(0);
+    setCandidateName("");
+    setCandidateEmail("");
+    setCandidateCv("");
+    setCandidateJobId(0);
+    setCandidateDialog(true);
+    setIsNewCandidate(true);
+  };
+
   const showAddDialog = () => {
     setAddDialog(true);
     setIsNewRecord(true);
   };
 
+  const sortFromBack = () => {
+    if (asc === 0) {
+      setAsc(1);
+    } else {
+      setAsc(0);
+    }
+    dispatch(jobpostingActions.getSortedJobPostings(asc));
+  };
+
   const showEditDialog = (rowData) => {
     setPostingId(rowData.id);
-    setPostingDescription(rowData.name);
-    setPostingDeadline(rowData.speed);
+    setPostingDescription(rowData.description);
+    setPostingDeadline({ date: new Date(rowData.deadline) });
     setAddDialog(true);
     setIsNewRecord(false);
   };
 
   const expand = (rowData) => {
+    setSelectedId(rowData.id);
     setDialog(true);
-    dispatch(candidateActions.getCandidates(rowData.id));
+    try {
+      dispatch(candidateActions.getCandidates(rowData.id));
+    } catch (e) {
+      console.log(e);
+    }
   };
+
+  useEffect(() => {
+    dispatch(jobpostingActions.getJobPostings());
+  }, [dispatch]);
 
   const hideDialog = () => {
     setDialog(false);
     setAddDialog(false);
     setPostingId(0);
     setPostingDescription("");
-    setPostingDeadline(0);
+    setPostingDeadline({ date: new Date() });
   };
   const handleDeleteClick = (rowData) => {
     dispatch(jobpostingActions.deleteJobPosting(rowData.id));
@@ -77,20 +134,73 @@ function JobsTable() {
       dispatch(
         jobpostingActions.addJobPosting({
           id: postingId,
-          name: postingDescription,
-          speed: postingDeadline,
+          description: postingDescription,
+          deadline: postingDeadline.date,
         })
       );
     } else {
       dispatch(
         jobpostingActions.updateJobPosting(postingId, {
           id: postingId,
-          name: postingDescription,
-          speed: postingDeadline,
+          description: postingDescription,
+          deadline: postingDeadline.date,
         })
       );
     }
   };
+  const startFilter = () => {
+    if (filter !== "") {
+    }
+  };
+
+  const handleCandidateDelete = (rowData) => {
+    dispatch(
+      candidateActions.deleteCandidate(rowData.jobPostingId, rowData.id)
+    );
+    dispatch(candidateActions.getCandidates(rowData.id));
+  };
+
+  const handleCandidateSaveClick = () => {
+    if (isNewCandidate) {
+      dispatch(
+        candidateActions.addCandidate(
+          {
+            id: candidateId,
+            name: candidateName,
+            cv: candidateCv,
+            email: candidateEmail,
+            jobPostingId: candidateJobId,
+          },
+          candidateJobId
+        )
+      );
+    } else {
+      dispatch(
+        candidateActions.updateCandidate(
+          candidateId,
+          {
+            id: candidateId,
+            name: candidateName,
+            cv: candidateCv,
+            email: candidateEmail,
+            jobPostingId: candidateJobId,
+          },
+          candidateJobId
+        )
+      );
+    }
+    dispatch(candidateActions.getCandidates(selectedId));
+  };
+
+  const candidateFooter = (
+    <div>
+      <Button
+        label="Save"
+        icon="pi pi-save"
+        onClick={handleCandidateSaveClick}
+      />
+    </div>
+  );
 
   const dialogFooter = (
     <div>
@@ -98,15 +208,139 @@ function JobsTable() {
     </div>
   );
 
+  const renderDialog = () => {
+    if (candidateList.length > 0) {
+      return (
+        <>
+          <DataTable value={candidateList} responsiveLayout="scroll">
+            <Column field="name" header="Name"></Column>
+            <Column field="email" header="Email"></Column>
+            <Column body={opsDetail}></Column>
+          </DataTable>
+          <Button
+            label="Add Candidate"
+            icon="pi pi-plus"
+            onClick={addCandidate}
+          ></Button>
+          <Dialog header="CV" visible={cvDialog} onHide={hideCv}>
+            {renderCV()}
+          </Dialog>
+          <Dialog
+            header="Candidate"
+            visible={candidateDialog}
+            onHide={hideCandidate}
+            footer={candidateFooter}
+          >
+            <div>
+              <InputText
+                placeholder="Id"
+                onChange={(evt) => setCandidateId(evt.target.value)}
+                value={candidateId}
+              />
+            </div>
+            <div>
+              <InputText
+                placeholder="Name"
+                onChange={(evt) => setCandidateName(evt.target.value)}
+                value={candidateName}
+              />
+            </div>
+            <div>
+              <InputText
+                placeholder="Email"
+                onChange={(evt) => setCandidateEmail(evt.target.value)}
+                value={candidateEmail}
+              />
+            </div>
+            <div>
+              <InputText
+                placeholder="Cv"
+                onChange={(evt) => setCandidateCv(evt.target.value)}
+                value={candidateCv}
+              />
+            </div>
+            <div>
+              <InputText
+                placeholder="JobPostingId"
+                onChange={(evt) => setCandidateJobId(evt.target.value)}
+                value={candidateJobId}
+              />
+            </div>
+          </Dialog>
+        </>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Nu exista candidati</h3>
+          <Button
+            label="Add Candidate"
+            icon="pi pi-plus"
+            onClick={addCandidate}
+          ></Button>
+          <Dialog
+            header="Candidate"
+            visible={candidateDialog}
+            onHide={hideCandidate}
+            footer={candidateFooter}
+          >
+            <div>
+              <InputText
+                placeholder="Id"
+                onChange={(evt) => setCandidateId(evt.target.value)}
+                value={candidateId}
+              />
+            </div>
+            <div>
+              <InputText
+                placeholder="Name"
+                onChange={(evt) => setCandidateName(evt.target.value)}
+                value={candidateName}
+              />
+            </div>
+            <div>
+              <InputText
+                placeholder="Email"
+                onChange={(evt) => setCandidateEmail(evt.target.value)}
+                value={candidateEmail}
+              />
+            </div>
+            <div>
+              <InputText
+                placeholder="Cv"
+                onChange={(evt) => setCandidateCv(evt.target.value)}
+                value={candidateCv}
+              />
+            </div>
+            <div>
+              <InputText
+                placeholder="JobPostingId"
+                onChange={(evt) => setCandidateJobId(evt.target.value)}
+                value={candidateJobId}
+              />
+            </div>
+          </Dialog>
+        </div>
+      );
+    }
+  };
+
   const opsDetail = (rowData) => {
     return (
       <>
-        <Button label="Edit" icon="pi pi-pencil" />
+        <Button label="CV" icon="pi pi-book" onClick={() => openCV(rowData)} />
+        <span> </span>
+        <Button
+          label="Edit"
+          icon="pi pi-pencil"
+          onClick={() => showCandidate(rowData)}
+        />
         <span> </span>
         <Button
           label="Delete"
           icon="pi pi-times"
           className="p-button p-button-danger"
+          onClick={() => handleCandidateDelete(rowData)}
         />
       </>
     );
@@ -147,23 +381,23 @@ function JobsTable() {
       >
         <div>
           <InputText
-            placeholder="id"
+            placeholder="Id"
             onChange={(evt) => setPostingId(evt.target.value)}
             value={postingId}
           />
         </div>
         <div>
           <InputText
-            placeholder="name"
+            placeholder="Description"
             onChange={(evt) => setPostingDescription(evt.target.value)}
             value={postingDescription}
           />
         </div>
         <div>
-          <InputText
-            placeholder="Deadline"
-            onChange={(evt) => setPostingDeadline(evt.target.value)}
-            value={postingDeadline}
+          <Calendar
+            inline
+            value={postingDeadline.date}
+            onChange={(e) => setDeadline(e.value)}
           />
         </div>
       </Dialog>
@@ -177,9 +411,19 @@ function JobsTable() {
         icon="pi pi-plus"
         onClick={showAddDialog}
       ></Button>
-      <Dialog visible={listDialog} onHide={hideDialog}>
+      <span> </span>
+      <Button label="Asc/Desc" onClick={sortFromBack}></Button>
+      <Dialog visible={listDialog} onHide={hideDialog} header="Candidates">
         {renderDialog()}
       </Dialog>
+      <span> </span>
+      <InputText
+        placeholder="Filter Description"
+        onChange={(evt) => {
+          setFilter(evt.target.value);
+          startFilter();
+        }}
+      />
     </div>
   );
 }
